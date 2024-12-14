7f0fe8ff278ecf54ac3ca27f3975b45093b0a726
Hasham Younas <hashamyounis9@gmail.com>
2024-12-14 18:04:46 +0000

    Added ANN

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import numpy as np
-import pandas as pd
-from sklearn.metrics import accuracy_score, auc
+from sklearn.metrics import auc
 from sklearn.model_selection import train_test_split
 from sklearn import svm
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
 
 
 # generating mock data
@@ -25,6 +29,9 @@ y = labels
 
 # split the data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
 
 
 svm_model = svm.SVC(kernel="linear", C=1.0)
@@ -40,16 +47,55 @@ new_sample = np.array([[170, 65]])
 prediction = svm_model.predict(new_sample)
 gender = 'M' if prediction[0] == 1 else 'F'
 
-print("Gender of new prediction is:", gender)
+print("SVM Prediction:", gender)
 
 
-# Calculating accuracy manually just for fun
-correction_predictions_count = 0
-for i in range(0, len(y_actual)):
-    if y_predicted[i] == y_actual[i]:
-        correction_predictions_count = correction_predictions_count + 1
+# # Calculating accuracy manually just for fun
+# correction_predictions_count = 0
+# for i in range(0, len(y_actual)):
+#     if y_predicted[i] == y_actual[i]:
+#         correction_predictions_count = correction_predictions_count + 1
 
-print("Total predictions are:", len(y_actual))
-print("Correct predictions are:", correction_predictions_count)
-acc_score = (correction_predictions_count / len(y_actual)) * 100
-print("Manual Accuracy Score:", acc_score)
+# print("Total predictions are:", len(y_actual))
+# print("Correct predictions are:", correction_predictions_count)
+# acc_score = (correction_predictions_count / len(y_actual)) * 100
+# print("Manual Accuracy Score:", acc_score)
+# auc_score = auc(y_actual, y_predicted)
+# print("Manual Accuracy Score:", auc_score)
+
+
+
+
+
+
+################################
+
+
+
+
+
+
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+ann_model = Sequential([
+    Dense(16, input_dim=2, activation='relu'),
+    Dense(8, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+ann_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+# print("Training the model...")
+ann_model.fit(x_train, y_train, epochs=50, batch_size=8, verbose=0)
+
+# print("Evaluating model")
+ann_model.evaluate(x_test, y_test, verbose=0)
+
+# new_sample = np.array([[170, 65]])
+# new_sample = scaler.transform(new_sample)
+prediction = ann_model.predict(new_sample)
+predicted_class = (prediction > 0.5).astype(int)
+
+gender = "M" if predicted_class[0][0] == 1 else "F"
+print("ANN prediction:", gender)
